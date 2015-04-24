9ef132695f31aa54960355400a392d37c4dfd7f0
Yamile Colque <yamile.colque@gmail.com>
2015-04-24 14:19:20 -0700

    first commit

---
+++ db/migrate/20150424183348_create_jobs.rb
@@ -0,0 +1,11 @@
+class CreateJobs < ActiveRecord::Migration
+  def change
+    create_table :jobs do |t|
+    	t.text :title
+    	t.text :description
+    	t.text :location
+    	t.integer :pay
+        t.timestamps
+    end
+  end
+end
